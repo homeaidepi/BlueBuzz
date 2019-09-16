db1ea5258744dab1f7266e5023eac418556e5d36
Matthew Vandergrift <matthew.vandergrift@ibm.com>
2019-09-16 00:28:17 -0400

    update connection strings

--- Cloud/Functions/Actions/PostComment.js
+++ Cloud/Functions/Actions/PostComment.js
@@ -1,6 +1,7 @@
 const mongodb = require('mongodb');
 const pkg = require('mongodb/package.json');
-const uri = 'mongodb://admin:FPSCRUSGLHMHFZFP@portal-ssl1430-52.bmix-dal-yp-0ada527c-7794-4fff-8db1-e965ddf8d2bb.3526967433.composedb.com:24325,portal-ssl1304-53.bmix-dal-yp-0ada527c-7794-4fff-8db1-e965ddf8d2bb.3526967433.composedb.com:24325/compose?authSource=admin&ssl=true'
+//const uri = 'mongodb://admin:FPSCRUSGLHMHFZFP@portal-ssl1430-52.bmix-dal-yp-0ada527c-7794-4fff-8db1-e965ddf8d2bb.3526967433.composedb.com:24325,portal-ssl1304-53.bmix-dal-yp-0ada527c-7794-4fff-8db1-e965ddf8d2bb.3526967433.composedb.com:24325/compose?authSource=admin&ssl=true'
+const uri = 'mongodb://ibm_cloud_36ebc60d_641a_42d5_918d_151d7eddc912:0d913498eb189bb0b73377126dc08ec14d6c7b33986172fa5306775e2b14947e@bec49fc5-5959-4818-8adf-f1388499ca11-0.2adb0220806343e3ae11df79c89b377f.databases.appdomain.cloud:32045,bec49fc5-5959-4818-8adf-f1388499ca11-1.2adb0220806343e3ae11df79c89b377f.databases.appdomain.cloud:32045/ibmclouddb?authSource=admin&replicaSet=replset&ssl=true'
 const driverVersion = pkg.version;
 
 async function main(params) {
@@ -21,7 +22,7 @@ async function main(params) {
 
     //delete any previous comments
     //await client.db('compose').collection('Feedback').deleteMany({"instanceId":params.instanceId, "deviceId":params.deviceId})
-    let result = await client.db('compose').collection('Feedback').insert(doc);
+    let result = await client.db('compose').collection('Feedback').insertOne(doc);
   
     return { body: { result: result, driverVersion } };
 }
